7dc0dc5db0a881dc86cdeb7eb6247b2e5dd4470e
Yuzhu720 <yuzhuzhang5@gmail.com>
2022-08-02 14:43:52 +0100

    first commit

---
+++ JS/2_变量.js
@@ -0,0 +1,15 @@
+// 声明变量
+// var age;
+// age = 18;
+// alert(age);
+//变量的初始化
+var name = "zyz";
+console.log(name);
+// 小作业：交换两个变量的值,借助中间值
+var a = 1,
+  b = 2,
+  temp;
+temp = a;
+a = b;
+b = temp;
+console.log(a, b);
